edd1a9908496b482093f191706701cc03a3481f4
xuanfu <15553626266@163.com>
2020-12-21 17:07:58 +0800

    第一步

--- src/main.js
+++ src/main.js
@@ -1,16 +1,2 @@
-// let student = {
-//     name:'某某',
-//     sex:'男',
-//     age:20
-// }
-//
-// Object.defineProperty(student,'age',{
-//     set(){
-//         console.log(2)
-//     },
-//     //
-//     get(newVal){
-//         console.log(1)
-//     }
-// })
+
 console.log('this is mains')
